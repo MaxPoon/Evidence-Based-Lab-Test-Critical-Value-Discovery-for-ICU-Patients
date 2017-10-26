a5203010f639bb709c7df342aa37b840c25ef85b
Max Poon <maxpanziyuan@gmail.com>
2017-10-26 22:29:58 +0800

    remove unused function

--- utils/queries.py
+++ utils/queries.py
@@ -8,32 +8,18 @@ def getEngine():
 	if engine:
 		return engine
 	try:
-		# TODO: get credentials from local config file
-		engine = create_engine('postgresql://maxpoonr@localhost:5432/mimic')
-		return engine
+		engine = create_engine('postgresql://maxpoon:@localhost:5432/mimic')
+		connection = engine.connect()
+		return connection
 	except:
 		print("Unable to connect to the database")
 		return None
 
-def getPatients():
-	engine = getEngine()
-	patients = pd.read_sql_query('SELECT * FROM patients;',con=engine)
-	return patients
-
-def getPatientsOfGender(gender):
-	engine = getEngine()
-	patients = pd.read_sql_query('SELECT * FROM patients WHERE gender = "{gender}";'.format(gender=gender),con=engine)
-	return patients
-
-def selectColumnsFromTable(columns, table):
-	engine = getEngine()
-	columnsStr = ' ,'.join(columns)
-	results = pd.read_sql_query('SELECT {columns} FROM {table};'.format(columns=columnsStr, table=table),con=engine)
-	return results
-
-def selectColumnsFromTableWithConditions(columns, table, conditions):
-	engine = getEngine()
-	columnsStr = ' ,'.join(columns)
-	conditionsStr = ' ,'.join(conditions)
-	results = pd.read_sql_query('SELECT {columns} FROM {table} WHERE {conditions};'.format(columns=columnsStr, table=table, conditions=conditionsStr),con=engine)
-	return results
+def getConnection():
+	try:
+		engine = getEngine()
+		connection = engine.connect()
+		return connection
+	except:
+		print("Unable to connect to the database")
+		return None
